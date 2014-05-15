618e20d596f9e815561adfd62466fbc712c68b27
Julio López Montalvo <ljuliom@gmail.com>
2014-05-15 12:23:53 -0500

    added jade compiler and livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,11 +39,42 @@ module.exports = function(grunt){
 			js: {
 			    files: 'development_files/*.js',
 			    tasks: ["jshint","concat","uglify"]
-			 }
+			 },
+			jade:{
+				files: 'development_files/*.jade',
+			    tasks: ["jade"]
+			}
 		},
 		karma:{
 			target:{
 				configFile: 'karma.conf.js'
+			}
+		},
+		connect: {
+			server: {
+		      options: {
+		      	hostname: 'localhost',
+		        port: 8000
+		      }
+		    }
+		},
+		open: {
+			dev : {
+		      path: 'http://localhost:8000'
+		    }
+		},
+		jade: {
+			compile: {
+				options:{
+					pretty: true,
+				},
+	            files: [{
+			      expand: true,
+			      cwd: 'development_files',
+			      src: [ '*.jade' ],
+			      dest: '',
+			      ext: '.html'
+			    }]
 			}
 		}
 	});
@@ -52,8 +83,11 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks("grunt-contrib-concat");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-open');
 	grunt.loadNpmTasks('grunt-karma');
 
-	grunt.registerTask("default", ["sass","jshint","concat","uglify","watch"]);
+	grunt.registerTask("default", ["jade","sass","jshint","concat","uglify","connect","open:dev","watch"]);
 	grunt.registerTask("test","karma");
 }
